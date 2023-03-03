4703070137e7a37809e3ebdefd66bb52a3379b48
mohamedderbali <mohamed.derbali@esprit.tn>
2023-03-03 16:50:55 +0100

    parking

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const http = require('http');
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const http = require("http");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
@@ -36,15 +36,18 @@ app.use(function(err, req, res, next) {
 
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-io.on("connection", (socket)=>{
-  console.log("socket is connected!")
-  socket.on("studentIn", (student)=>{
+io.on("connection", (socket) => {
+  console.log("socket is connected!");
+  socket.on("studentIn", (student) => {
     console.log(`student ${student} In class!`);
     socket.emit("studentInClass", student);
   });
-  
-})
+  socket.on("parking", (car) => {
+    console.log(car);
+    socket.emit("carParked", car);
+  });
+});
 
-server.listen(5000, ()=>{
+server.listen(5000, () => {
   console.log("app is running on port 5000");
 });
